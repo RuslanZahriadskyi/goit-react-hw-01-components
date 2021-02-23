d9a9e7623cb21786d10667fddaa878656bb37ad4
Ruslan Zahriadskyi <67427154+RuslanZahriadskyi@users.noreply.github.com>
2021-02-23 19:48:26 +0100

    changes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import s from './Statistics.module.css';
 import PropTypes from 'prop-types';
 import StatisticsList from './StatisticsList';
 
-function Statistics({ title = '', items }) {
+function Statistics({ title, items }) {
   return (
     <section className={s.section}>
       {title && <h2 className={s.title}>{title}</h2>}
